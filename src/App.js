fd3e69505597ec531b3246519e009bf559ad1f33
BuildTools <gytis.ne.gal@gmail.com>
2023-12-15 20:45:07 +0200

    The paper

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+          <div id="TITILE">Daugiau nieko nera</div>
+          <img src="paper.png" id="rotatable-image" width='400' height='auto'  alt="bonka a"></img>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+
